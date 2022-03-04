f0d0afa4d232b44f18b02a649935407ec0e2fe6f
Josué Rodríguez-Ramos <josue.a.rodriguez.ramos@gmail.com>
2022-03-03 18:27:58 -0700

    Update and rename 3_vMAG_identification_quality_annotations_and_read_mapping.txt to 3_vMAG_processing.sh

--- 3_vMAG_processing.sh
+++ 3_vMAG_processing.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
+##Identify viruses from assembled metaG.
 #VirSorter 1.0.3 was run on CyVerse Discovery Environment. Briefly, metagenomes were screened for DNA viral sequences using VirSorter v1.0.3 with the ViromeDB database option (65), retaining viral contigs ranked 1, 2, 4 or 5 where category 1-2 indicate high confidence predicted lytic viruses and 4-5 indicate high-confidence prophage sequences from VirSorter output.
 
 #Clustering of VirSorter identified viral contigs
 Cluster_genomes_5.1.pl -f unclustered_viruses_ge10kb.fasta -c 85 -i 95
 
 #Annotation of vMAGs with DRAM-V
-
 #First we remove "bad" characters from fasta of concatenated, clustered viruses ge10kb:
 DRAM-v.py remove_bad_characters -i clustered_vMAGs_95-85.fasta -o clustered_vMAGs_95-85_nobadchars.fasta
 
@@ -16,10 +18,12 @@ DRAM-v.py annotate -i clustered_vMAGs_95-85_nobadchars.fasta -v viral-affi-conti
 
 DRAM-v.py distill -i DRAMv_output/annotations.tsv -o /DRAMv_distill
 
-#vContact2 was run on CyVerse Discovery Environment. This was done on software version 0.9.8 with default settings. Files that were used to run this were deposited on Zenodo under DOI: 10.5281/zenodo.6310084
-
 #Phyre2 analyses were done on the Phyre2 online servers (http://www.sbg.bio.ic.ac.uk/phyre2/html/page.cgi?id=index) by uploading an amino acid sequence containing AMGs of interest.
 
+##Viral Taxonomy
+#vContact2 was run on CyVerse Discovery Environment. This was done on software version 0.9.8 with default settings. Files that were used to run this were deposited on Zenodo under DOI: 10.5281/zenodo.6310084
+
+##Virus - Host Identification
 #After both hosts and viruses were acquired, we then run VirHostMatcher on viral and microbial genomes to determine host predictions:
 python /opt/VirHostMatcher/vhm.py -v virus_genomes_dir -b host_genomes_dir -o output_virhostmatcher_results
 
